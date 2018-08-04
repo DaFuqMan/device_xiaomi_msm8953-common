Yash089610/android_device_xiaomi_msm8953-common

XiaomiParts : Enhancements and improvements 1) Call a service from BroadcastReceiver to restore values 2) Add user selectable delay to restore values after boot
Signed-off-by: sayan7848 <sayan7848@gmail.com>

XiaomiParts/src/com/screwd/settings/device/RestoreService.java
@@ -1,0 +1,32 @@
+package com.screwd.settings.device;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class RestoreService extends IntentService {
+
+		private static final String TAG = "RestoreService";
+
+		public RestoreService() {
+      super(RestoreService.class.getName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        VibratorStrengthPreference.restore(this);
+        WhiteTorchBrightnessPreference.restore(this);
+        YellowTorchBrightnessPreference.restore(this);
+        
+        KcalRGBMinPreference.restore(this);
+        KcalSatIntensityPreference.restore(this);
+        KcalScreenHuePreference.restore(this);
+        KcalScreenValuePreference.restore(this);
+        KcalScreenContrPreference.restore(this);
+
+        KcalUtils.restoreRGBAfterBoot(this);
+ 
+    }
+}

DaFuqMan/device_xiaomi_msm8953-common

XiaomiParts/src/com/endless/settings/device/RestoreService.java
@@ -1,0 +1,32 @@
+package com.endless.settings.device;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class RestoreService extends IntentService {
+
+		private static final String TAG = "RestoreService";
+
+		public RestoreService() {
+      super(RestoreService.class.getName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        VibratorStrengthPreference.restore(this);
+        WhiteTorchBrightnessPreference.restore(this);
+        YellowTorchBrightnessPreference.restore(this);
+        
+        KcalRGBMinPreference.restore(this);
+        KcalSatIntensityPreference.restore(this);
+        KcalScreenHuePreference.restore(this);
+        KcalScreenValuePreference.restore(this);
+        KcalScreenContrPreference.restore(this);
+
+        KcalUtils.restoreRGBAfterBoot(this);
+ 
+    }
+}