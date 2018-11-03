5f561479029f907c71da7cd4ad073ae717f8a81b
Sagar Rokade <sagarrokade006@gmail.com>
2018-11-03 03:49:14 +0000

    Bringup For Xtended

--- prop.mk
+++ prop.mk
@@ -184,3 +184,9 @@ persist.sys.usb.config.extra=none
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
+
+#Xtended
+ro.xtended.version=$(XTENDED_XTRA_VERSION) \ 
+ro.modversion=$(XTENDED_MOD_VERSION) \ 
+ro.xtended.buildtype=$(XTENDED_BUILD_TYPE) \ 
+ro.xtended.display.version=$(XTENDED_DISPLAY_VERSION)
