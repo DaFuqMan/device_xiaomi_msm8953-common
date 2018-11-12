cce1778676ea5af35ba0376f33412c23eccff0df
Sagar Rokade <sagarrokade006@gmail.com>
2018-11-12 06:37:43 +0000

    Fix MIUI Offline Charging

--- msm8953.mk
+++ msm8953.mk
@@ -195,6 +195,10 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-impl \
     android.hardware.health@2.0-service
 
+# Charging
+PRODUCT_PACKAGES += \
+    chargeonlymode
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
